1e0638578fc432dd397ac1a2fc77d68bf369f0d1
Utkarsh <famedustevershine@gmail.com>
2023-04-11 00:37:24 +0530

    h

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const io = require("socket.io")(8900, {
-    // cors: {
-    //   origin: "http://localhost:3000",
-    // },
+    cors: {
+      origin: "http://localhost:3000",
+    },
 });
 
 
 let users = [];
+
 
 
 const addUser = (userId, socketId) => {
