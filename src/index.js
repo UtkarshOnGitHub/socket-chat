18f4aaf42c13f431002228a7bfe5b76e0f0c1553
Utkarsh <famedustevershine@gmail.com>
2023-04-11 21:54:29 +0530

    .

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@
 //     },
 // });
 
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
+const express = require("express")
+const http = require("http")
+const socketIO = require("socket.io")
+const cors = require("cors")
 
 const app = express();
+app.use(cors())
 const server = http.createServer(app);
 const io = socketIO(server);
 
